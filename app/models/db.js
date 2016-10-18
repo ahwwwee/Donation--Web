05b1016eb396b399a81698f010620d6cb215f1cd
Ari <ahwwwee@gmail.com>
2016-10-18 14:09:14 +0100

    delete all tweets working

--- app/models/db.js
+++ app/models/db.js
@@ -21,3 +21,20 @@ mongoose.connection.on('error', function (err) {
 mongoose.connection.on('disconnected', function () {
   console.log('Mongoose disconnected');
 });
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+  if (process.env.NODE_ENV != 'production') {
+    var seeder = require('mongoose-seeder');
+    const data = require('./initdata.json');
+    const Donation = require('./donation');
+    const User = require('./user');
+    const Candidate = require('./candidate.js');
+    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
+      console.log(dbData);
+    }).catch(err => {
+      console.log(error);
+    });
+  }
+});
